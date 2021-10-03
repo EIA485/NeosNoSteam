4ae9e2f819b3aff088004dcb22bc463ef7183bc1
EIA485 <17285570+EIA485@users.noreply.github.com>
2021-10-02 17:53:25 -0700

    Add project files.

---
+++ NoSteam/NoSteam.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using BaseX;
+using System;
+using CloudX.Shared;
+
+namespace NoSteam
+{
+    public class NoSteam : NeosMod
+    {
+        public override string Name => "NoSteam";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosNoSteam/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.NoSteam");
+            harmony.PatchAll();
+        }
+        [HarmonyPatch(typeof(SteamConnector))]
+        class NoSteamPatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("InitializeSteamAPI")]
+            static bool InitializeSteamAPIPrefix()
+            {
+                return false;
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch("InitializationAttempted", MethodType.Getter)]
+            static bool InitializationAttemptedPrefix(ref bool __result)
+            {
+                __result = true;
+                return false;
+            }
+        }
+    }
+}
